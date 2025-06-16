897fe29b64535ea4610f95acd1cd44618391faa5
Hanu Shashwat <hanushashwat733@gmail.com>
2025-06-16 18:01:48 +0530

    Day 3

---
+++ lib/pages/note.dart
@@ -0,0 +1,65 @@
+/*body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25.0, top: 12, right: 25),
+          child: Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Need Help?',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Color.fromRGBO(8, 129, 208, 1),
+                  ),
+                ),
+
+                TextSpan(
+                  text: ' You’re in the Right Place.\n',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+
+                TextSpan(
+                  text: 'We’re here to support you—every step of the way.\n',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+
+                WidgetSpan(
+                    child: SizedBox(
+                      height: 80,
+                    )
+                ),
+
+                TextSpan(
+                  text: 'Ways to Reach Us\n',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+
+                WidgetSpan(
+                    child: SizedBox(
+                      height: 80,
+                    )
+                ),
+
+                TextSpan(
+                  text: '1. WhatsApp',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+              ],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),*/
--- lib/pages/support.dart
+++ lib/pages/support.dart
@@ -34,13 +34,95 @@ class Support extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text(
-            'Support Page',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blue,
-            ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 25.0, top: 18, right: 25),
+          child: Column(
+            children: [
+              Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                    children: [
+                      TextSpan(
+                        text: 'Need Help?',
+                        style: TextStyle(
+                          color: Color.fromRGBO(8, 129, 208, 1),
+                        ),
+                      ),
+                      TextSpan(
+                        text: " You're at the Right Place.\nWe’re here to support you—every step of the way.",
+                        style: TextStyle(
+                          color: Colors.grey.shade800,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "Ways To Reach Us:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[800],
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 15,
+              ),
+
+              Align(
+                alignment: Alignment.topLeft,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      'lib/images/whatsapp.png',
+                      height: 28,
+                      width: 28,
+                    ),
+                    SizedBox(width: 8),
+                    RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 18,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "WhatsApp - ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          TextSpan(
+                            text: "Click here or message\nus on +91-6201668873.",
+                            style: TextStyle(
+                              color: Colors.grey[800],
+                            )
+                          )
+                        ]
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],
           ),
         ),
       ),
